1438d55438e3192a7e92374939aeb63e051fe178
Lennox <omondilenox@outlook.com>
2020-08-10 09:38:40 +0300

    Updates

---
+++ precache-manifest.27f397c583e938424ae2441c8eaa10dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc10a5436e413c539965df9833dafca",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "0c6896e1662cd66bd924",
+    "url": "/pizza/static/css/main.fedb3be0.chunk.css"
+  },
+  {
+    "revision": "67e4a06ad7ff46cff5df",
+    "url": "/pizza/static/js/2.e932bb21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza/static/js/2.e932bb21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6896e1662cd66bd924",
+    "url": "/pizza/static/js/main.bf711f18.chunk.js"
+  },
+  {
+    "revision": "d62fa667c74fd46d57c6",
+    "url": "/pizza/static/js/runtime-main.48791bff.js"
+  }
+]);
