8295c377b1f77a2e374acc72ad6d836b8e0e60d7
Lennox <omondilenox@outlook.com>
2020-08-10 08:59:28 +0300

    removed router import prop from product card

--- src/components/ProductCard/ProductCard.component.jsx
+++ src/components/ProductCard/ProductCard.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link } from "react-router-dom";
 
-const ProductCard = ({ product,router }) => {
+const ProductCard = ({ product}) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img className="card-img-top" src={product.image} alt="Card image cap" />
